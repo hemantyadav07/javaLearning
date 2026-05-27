af347b70119b88163fd96c460b92ad3c6559f552
hemantyadav07 <hemanttyadav007@gmail.com>
2026-05-27 23:43:26 +0530

    loop

---
+++ loop.java
@@ -0,0 +1,13 @@
+public class loop {
+    public static void main(String[] args) {
+        int a [] = {1,2,3,4,5};
+        int s = 0;
+        for(int x:a){
+            s=s+x;
+        }
+        System.out.println(s);
+        }
+
+        }
+    
+
---
+++ marks.java
@@ -0,0 +1,16 @@
+public class marks{
+    public static void main(String[] args) {
+        int m=85;
+        if(m >90){
+            System.out.println("grade A");
+        }
+        else if(m >80  && m <90){
+            System.out.println("grade B");
+        }
+        else
+        {
+            System.out.println("grade C");
+        }
+
+    }
+}
